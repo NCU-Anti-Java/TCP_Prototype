24f715bac8dd4c426601b584e185276c43e5ab4c
tjjh89017 <tjjh89017@hotmail.com>
2015-10-27 19:29:33 +0800

    fix

--- src/main/java/com/github/tjjh89017/TCP_Prototype/Client.java
+++ src/main/java/com/github/tjjh89017/TCP_Prototype/Client.java
@@ -1,11 +1,5 @@
 package com.github.tjjh89017.TCP_Prototype;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.channel.Channel;
-
 
 public class Client {
 
@@ -13,12 +7,6 @@ public class Client {
     private String name = null;
     private String host = "127.0.0.1";
     private int port = 5566;
-
-    private EventLoopGroup eventLoopGroup;
-    private Channel channel;
-    private Bootstrap bootstrap;
-
-    private ClientEvent clientEvent = new ClientEvent();
 
     public Client(){
 
@@ -39,16 +27,10 @@ public class Client {
 
     public void connect() throws InterruptedException{
 
-        eventLoopGroup = new NioEventLoopGroup();
-
-        bootstrap = new Bootstrap();
-        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).handler(new PRGClientInitializer(clientEvent));
-
-        channel = bootstrap.connect(host, port).sync().channel();
+       
     }
 
     public void close() {
 
-        eventLoopGroup.shutdownGracefully();
     }
 }
--- src/main/java/com/github/tjjh89017/TCP_Prototype/ServerEvent.java
+++ src/main/java/com/github/tjjh89017/TCP_Prototype/ServerEvent.java
@@ -24,7 +24,7 @@ public class ServerEvent {
         int id = StringToInt(result[1]);
         System.out.println(ctx.toString());
 
-        
+
         synchronized (this) {
             if (result[0].equals(GET)) {
                 // check if it could
